2c58b90d8abdd2a92399e86a950665c1c525c690
Sky <Sky@BLAZEPC3>
2018-12-15 23:04:04 +0100

    Filling Serialize method

--- TestChars/TestChars/Helpers/Serializer.cs
+++ TestChars/TestChars/Helpers/Serializer.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+using TestChars.Models;
+using Newtonsoft.Json;
 
-namespace TestChars.Helpers
+namespace JsonTest.Helpers
 {
-    class Serializer
+    public class Serializer
     {
-        //extra strings were removed
+
+        public static void SerializerMethod2()
+        {
+            string JsonString = JsonConvert.SerializeObject(new Customer()
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Rambo",
+                Age = 70
+            });
+            Console.WriteLine(JsonString);
+            Console.ReadLine();
+        }
+
+        public static void SerializerMethod1()
+        {
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            string JsonString = ser.Serialize(new Customer()
+            {
+                Id = 2,
+                FirstName = "John",
+                LastName = "Rambo",
+                Age = 70
+            });
+            Console.WriteLine(JsonString);
+            Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Sky\source\repos\JsonTest\JsonTest\Data\output.json", JsonString);
+        }
+
     }
 }
+
